7c515f6d305d8270730e211520f17494b1f7c176
LiamRICE <71870225+LiamRICE@users.noreply.github.com>
2021-12-27 15:02:59 +0100

    split etl and app programs

--- app.py
+++ app.py
@@ -6,114 +6,51 @@ from dash import html
 import plotly.express as px
 import pandas as pd
 
+print("Reading raw data...")
 df = pd.read_csv('data_balea.csv', sep=';')
+newDF = pd.read_csv('data_trucks.csv', sep=',')
+figDF = pd.read_csv('data_fig.csv', sep=',')
+print("Data read.")
 
-comboListTAR = []
-for i in range(0, len(df)):
-    comboListTAR.append((df.TruckName[i], df.TARException[i]))
-
-comboListFlag = []
-for i in range(0, len(df)):
-    comboListFlag.append((df.TruckName[i], df.TARException[i], df.FlagWeightOverriden[i]))
-
-comboListWeights = []
-for i in range(0, len(df)):
-    theoWeight = df.WeightProductTradeUnitUsed[i]
-    if df.Quantity[i] != 0:
-        measuredWeight = df.Weight[i] / df.Quantity[i]
-        if theoWeight != 0:
-            comboListWeights.append((df.TruckName[i], (measuredWeight - theoWeight)/theoWeight, df.Weight[i]))
-        else:
-            comboListWeights.append((df.TruckName[i], 0, df.Weight[i]))
-    else:
-        measuredWeight = df.Weight[i]
-        if theoWeight != 0:
-            comboListWeights.append((df.TruckName[i], (measuredWeight - theoWeight)/theoWeight, df.Weight[i]))
-        else:
-            comboListWeights.append((df.TruckName[i], 0, df.Weight[i]))
-
-
-listOfTrucks = []
-for truck in df.TruckName:
-    if truck not in listOfTrucks:
-        listOfTrucks.append(truck)
-
-countTarExceptions = []
-for truck in listOfTrucks:
-    count = 0
-    for name,exp in comboListTAR:
-        if name == truck:
-            if exp == 1:
-                count += 1
-    countTarExceptions.append(count)
-
-missionsOfTruck = []
-for truck in listOfTrucks:
-    count = 0
-    for name,cnt in comboListTAR:
-        if name == truck:
-            count += 1
-    missionsOfTruck.append(count)
-
-exceptionRate = []
-for i in range(0, len(countTarExceptions)):
-    exceptionRate.append(countTarExceptions[i]/missionsOfTruck[i])
-
-flagTARCorrelation = []
-for truck in listOfTrucks:
-    countEquals = 0
-    countTotal = 0
-    for name, e, f in comboListFlag:
-        if truck == name:
-            countTotal += 1
-            if e == f:
-                countEquals += 1
-    flagTARCorrelation.append(countEquals/countTotal)
-
-ratWeight = []
-negWeight = []
-for truck in listOfTrucks:
-    cnt = 0
-    totalWeightDiv = 0
-    cntNegWeight = 0
-    for name, rat, wgt in comboListWeights:
-        if truck == name:
-            cnt += 1
-            totalWeightDiv += rat
-            if wgt <= 0:
-                cntNegWeight += 1
-    ratWeight.append(totalWeightDiv / cnt)
-    negWeight.append(cntNegWeight)
-
-zipped = list(zip(listOfTrucks, countTarExceptions, missionsOfTruck, exceptionRate, flagTARCorrelation, ratWeight, negWeight))
-newDF = pd.DataFrame(zipped, columns=['Trucks', 'TARExceptions', 'NumMissions', 'ExceptionRate', 'TARCorrelation', 'AvgWeightDiff', 'NumNegativeWeights'])
-
-dfHead = df.head()
+# make figure
+fig = px.scatter(figDF, x="missions", y="exceptions", color="names", hover_name="names", log_x=True)
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(children=[
     html.H1(children='Balea Data Analysis'),
 
-    html.Div(children='''
-        Header table of the raw data.
-    '''),
+    dcc.Tabs([
+        dcc.Tab(label='Raw Data', children=[
+            html.Div(children='''
+                Header table of the raw data.
+            '''),
+            dash_table.DataTable(
+            id = 'table-head',
+            columns = [{"name":i, "id":i} for i in df.columns],
+            data = df.to_dict('records'),
+            page_size=50
+            )
+        ]),
 
-    dash_table.DataTable(
-    id = 'table-head',
-    columns = [{"name":i, "id":i} for i in dfHead.columns],
-    data = dfHead.to_dict('records'),
-    ),
-
-    html.Div(children='''
-        Synthesised table of data for each truck.
-    '''),
-
-    dash_table.DataTable(
-    id = 'new-table',
-    columns = [{"name":i, "id":i} for i in newDF.columns],
-    data = newDF.to_dict('records'),
-    )
+        dcc.Tab(label='Truck Data', children=[
+            html.Div(children='''
+                Synthesised table of data for each truck.
+            '''),
+            dash_table.DataTable(
+            id = 'new-table',
+            columns = [{"name":i, "id":i} for i in newDF.columns],
+            data = newDF.to_dict('records')
+            )
+        ]),
+        
+        dcc.Tab(label='Exceptions Graph', children=[
+            dcc.Graph(
+                id='graph-of-new-data',
+                figure=fig
+            )
+        ]),
+    ])
 ])
 
 if __name__ == '__main__':
---
+++ etl.py
@@ -0,0 +1,90 @@
+import pandas as pd
+
+print("Reading raw data...")
+df = pd.read_csv('data_balea.csv', sep=';')
+print("Data read.")
+
+# creating lists of proto-data
+print("Preparing proto-data...")
+comboListTAR = []
+comboListFlag = []
+comboListWeights = []
+for i in range(0, len(df)):
+    comboListFlag.append((df.TruckName[i], df.TARException[i], df.FlagWeightOverriden[i]))
+    comboListTAR.append((df.TruckName[i], df.TARException[i]))
+    theoWeight = df.WeightProductTradeUnitUsed[i]
+    if df.Quantity[i] != 0:
+        measuredWeight = df.Weight[i] / df.Quantity[i]
+        if theoWeight != 0:
+            comboListWeights.append((df.TruckName[i], (measuredWeight - theoWeight)/theoWeight, df.Weight[i]))
+        else:
+            comboListWeights.append((df.TruckName[i], 0, df.Weight[i]))
+    else:
+        measuredWeight = df.Weight[i]
+        if theoWeight != 0:
+            comboListWeights.append((df.TruckName[i], (measuredWeight - theoWeight)/theoWeight, df.Weight[i]))
+        else:
+            comboListWeights.append((df.TruckName[i], 0, df.Weight[i]))
+print("Proto-data prepared.")
+
+# creating list of trucks (individuals)
+print("Preparing list of trucks...")
+listOfTrucks = []
+for truck in df.TruckName:
+    if truck not in listOfTrucks:
+        listOfTrucks.append(truck)
+print("List of trucks prepared.")
+
+# initialising new data lists
+print("Preparing truck data...")
+countTarExceptions = []
+missionsOfTruck = []
+exceptionRate = []
+flagTARCorrelation = []
+ratWeight = []
+negWeight = []
+
+for truck in listOfTrucks:
+    nbExp = 0
+    nbMissions = 0
+    countEquals = 0
+    countTotal = 0
+    cnt = 0
+    totalWeightDiv = 0
+    cntNegWeight = 0
+    for name,exp in comboListTAR:
+        if name == truck:
+            nbMissions += 1
+            if exp == 1:
+                nbExp += 1
+    for name, e, f in comboListFlag:
+        if truck == name:
+            countTotal += 1
+            if e == f:
+                countEquals += 1
+    for name, rat, wgt in comboListWeights:
+        if truck == name:
+            cnt += 1
+            totalWeightDiv += rat
+            if wgt <= 0:
+                cntNegWeight += 1
+    flagTARCorrelation.append(countEquals/countTotal)
+    countTarExceptions.append(nbExp)
+    missionsOfTruck.append(nbMissions)
+    ratWeight.append(totalWeightDiv / cnt)
+    negWeight.append(cntNegWeight)
+
+for i in range(0, len(countTarExceptions)):
+    exceptionRate.append(countTarExceptions[i]/missionsOfTruck[i])
+print("Truck data prepared.")
+
+# making new dataframe with trucks as individuals
+print("Creating truck dataframe...")
+zipped = list(zip(listOfTrucks, countTarExceptions, missionsOfTruck, exceptionRate, flagTARCorrelation, ratWeight, negWeight))
+newDF = pd.DataFrame(zipped, columns=['Trucks', 'TARExceptions', 'NumMissions', 'ExceptionRate', 'TARCorrelation', 'AvgWeightDiff', 'NumNegativeWeights'])
+zipped = list(zip(listOfTrucks, missionsOfTruck, exceptionRate, ratWeight))
+figDF = pd.DataFrame(zipped, columns=['names', 'missions', 'exceptions', 'weight-diff'])
+print("Truck dataframe created.")
+
+newDF.to_csv('data_trucks.csv')
+figDF.to_csv('data_fig.csv')
